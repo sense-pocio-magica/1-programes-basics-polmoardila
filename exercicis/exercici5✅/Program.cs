1584096ae79954a92c9751a32139168e2082e58f
polmoardila <polmoardila@cendrassos.net>
2025-10-01 13:06:29 +0200

    fins ex9

---
+++ exercicis/exercici5✅/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace exercici5;
+
+/* Feu un programa que demani un nom i l’any de naixement i imprimeixi per pantalla 
+“Hola [nom]!  Ja tens [edat] anys? 
+nom i edat seran el nom introduït i l'edat la resta de l’any actual i l’any de naixement. */
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digue'm el teu nom:");
+        var nom = Console.ReadLine();
+
+        Console.WriteLine("Digue'm el teu any de neixament:");
+        var data = Console.ReadLine();
+        int data2 = Convert.ToInt16(data);
+
+        int edat = 2025 - data2;
+        
+        Console.WriteLine($"Hola {nom}! Ja tens {edat} anys?");
+    }
+}
---
+++ exercicis/exercici8 revisar/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace exercici8;
+
+/* Assigna un nombre total de minuts a una variable. 
+Calcula quantes hores senceres i minuts restants són i imprimeix el resultat. */
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Calcul de minuts a continuació introdueix els minuts que vols passar a hores:");
+        var minuts = Console.ReadLine();
+        int minutsint = Convert.ToInt16(minuts);
+
+        int hores = minutsint / 60 ;
+        int totalminuts = minutsint % 60;
+        Console.WriteLine($"Això són {hores} hores i {minuts} minuts.");
+
+    }
+}
